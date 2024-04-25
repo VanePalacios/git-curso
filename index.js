52166f7748bc939cca379618a95b42e62664f8e2
VanePalacios <127250796+VanePalacios@users.noreply.github.com>
2024-04-25 13:26:06 -0600

    Update index.js (#1)

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const log = require('loglevel');
 function funcion() {
   console.log("Ejemplo")
 }
+function VanessaAguilar() {console.log('Vanessa Aguilar')};
 const main = () => {
   console.log('Inicio')
   log.warn("unreasonably simple");
-  funcion()
+  funcion();
+  VanessaAguilar();
 }
 main();
